0a55a0b017f2902e735d2a1b351678e3d80d9b78
Giridharan <52557361+giridharan25@users.noreply.github.com>
2020-01-19 22:01:04 +0530

    Update amzon_price_checker.py

--- amzon_price_checker.py
+++ amzon_price_checker.py
@@ -27,7 +27,7 @@ def check_price():
     
     title=soup.find(id='productTitle').get_text()
     
-    price=soup.find(id='priceblock_ourprice').get_text()
+    price=soup.find(id='priceblock_dealprice').get_text()
     
     converted_price=price[2:]
     
